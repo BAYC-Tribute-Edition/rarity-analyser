ab7272147928abc2e90c6328a3b38edcc9c80995
Gajche <32923922+Gajche@users.noreply.github.com>
2022-02-17 21:01:12 +0100

    heroku

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 const config = {
     app_name: 'Cool Rarity',
     app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
-    collection_file_name: 'collection.json',
-    collection_contract_address: '0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91',
-    collection_name: '10k One Day Punks',
-    collection_description: '10k "One Day I\'ll Be A Punk"-punks – a homage to the one and only CryptoPunks. Holding a OneDayPunk will give you early access to PunkScapes and reserve a profile on the PunkScape website.',
+    collection_file_name: '_metadata.json',
+    collection_contract_address: '0x19eB8A3eCD7e7e377E07de339C03c61931CFE543',
+    collection_name: 'Bored Ape Yacht Club TRIBUTE EDITION',
+    collection_description: 'BAYC Tribute Edition is a collection of 10,008 Tribute Bored Ape NFTs—unique digital collectibles living on the Polygon blockchain. They all have unique properties. NOT A COPY PASTE EVERYTHING IS HAND DRAWN, FROM SCRATCH',
     collection_id_from: 0,
     ignore_traits: ['date'], 
-    sqlite_file_name: 'database.sqlite',
+    sqlite_file_name: 'database.bayct.db',
     ga: 'G-BW69Z04YTP',
-    main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/og.png',
-    item_path_name: 'punk',
-    page_item_num: 60,
+    main_og_image: 'https://lh3.googleusercontent.com/kWdK4fVU2WpsnkB4uKzg5MMwrybN0jlJ7RFraDcTji2qKeC60BJTBIXTO83wMo7yKc0hrMHz63kLd1y4t_wr8iWL1xoZjVu8nVpF=w600',
+    item_path_name: 'bayct',
+    page_item_num: 50,
     content_image_is_video: false,
-    content_image_frame: 'circle', // circle, rectangle
+    content_image_frame: 'rectangle', // circle, rectangle
     use_wallet: false
 };
 
